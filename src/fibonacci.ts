d9f42cb281f55e0236eb5429948c8bc4f3a3e8ab
Jan Straka <xstraka8@fi.muni.cz>
2024-09-23 13:33:00 +0200

    break: type checking

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -5,7 +5,7 @@ export function fibonacci(i: number): number {
   if (i == 0) {
     return 0;
   }
-  if (i == 1) {
+  if (i == "1") {
     return 1;
   }
   return fibonacci(i - 1) + fibonacci(i - 2);
